b5f4f5528e96b46dae93c4dfe8e6fdd0955d99a5
PetroniusA <alesfiser6@gmail.com>
2024-05-11 13:00:14 +0200

    Added Hellow world

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# view defined using function
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def hello2(request, str):
+    return HttpResponse(f'Hello {str} world')
+
+def hello3(request):
+    s = request.GET.get("str" , "")
+    return HttpResponse(f'Hello , {s}, world')
+
